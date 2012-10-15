219ccb8481ab012d2f3cce24da3f408bf9de9812
Kevron Rees <kevron_m_rees@linux.intel.com>
2012-10-15 14:19:16 -0700

    added light status and updated fuel docs

--- plugins/dbus/dbusplugin.cpp
+++ plugins/dbus/dbusplugin.cpp
@@ -28,7 +28,7 @@ extern "C" AbstractSinkManager * create(AbstractRoutingEngine* routingengine, ma
 	return new DBusSinkManager(routingengine, config);
 }
 
-DBusSink::DBusSink(string interface, string path, AbstractRoutingEngine* engine, GDBusConnection* connection, map<string, string> config)
+DBusSink::DBusSink(string interface, string path, AbstractRoutingEngine* engine, GDBusConnection* connection, map<string, string> config = map<string, string>())
 	:AbstractDBusInterface(interface, path, connection),
 	  AbstractSink(engine, config), supported(false)
 {
