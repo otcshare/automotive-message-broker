697f030369b6552feec6f51aac361eb660f97078
Kevron Rees <kevron.m.rees@intel.com>
2013-09-03 16:13:28 -0700

    fixed getRanged for new properties syntax

--- plugins/websocketsourceplugin/websocketsource.cpp
+++ plugins/websocketsourceplugin/websocketsource.cpp
@@ -546,16 +546,6 @@ void WebSocketSource::unsubscribeToPropertyChanges(VehicleProperty::Property pro
 
 void WebSocketSource::getPropertyAsync(AsyncPropertyReply *reply)
 {
-	///TODO: fill in
-	//s << "{\"type\":\"method\",\"name\":\"getSupportedEventTypes\",\"data\":[],\"transactionid\":\"" << "d293f670-f0b3-11e1-aff1-0800200c9a66" << "\"}";
-	//m_re->getPropertyAsync();
-	/*reply.value = 1;
-	  reply->completed(reply);
-	  reply->completed = [](AsyncPropertyReply* reply) {
-	  DebugOut()<<"Velocity Async request completed: "<<reply->value->toString()<<endl;
-	  delete reply;
-	};*/
-	//propertyReplyMap[reply->property] = reply;
 	std::string uuid = amb::createUuid();
 	uuidReplyMap[uuid] = reply;
 	uuidTimeoutMap[uuid] = amb::currentTime() + 10.0; ///TODO: 10 second timeout, make this configurable?
@@ -581,8 +571,21 @@ void WebSocketSource::getRangePropertyAsync(AsyncRangePropertyReply *reply)
 	s.precision(15);
 	s << "{\"type\":\"method\",\"name\":\"getRanged\",\"data\": {";
 
-	///TODO: fix me.  ranged requests over websocket source will be broken
-	//s << "\"property\":\""<< reply->property << "\",";
+	s << "\"properties\":[";
+
+	for (auto itr = reply->properties.begin(); itr != reply->properties.end(); itr++)
+	{
+		std::string prop = *itr;
+
+		if(itr != reply->properties.begin())
+		{
+			s<<",";
+		}
+
+		s<<"\""<<prop<<"\"";
+	}
+
+	s<<"],";
 
 	s << "\"timeBegin\":\"" << reply->timeBegin << "\",";
 	s << "\"timeEnd\":\"" << reply->timeEnd << "\",";
