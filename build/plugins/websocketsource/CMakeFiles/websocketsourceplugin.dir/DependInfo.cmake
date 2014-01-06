8e5b7528f3848a266aa44b5e299f64b5898cfe8a
Kevron Rees <kevron.m.rees@intel.com>
2014-01-06 14:31:49 -0800

    updated examples

---
+++ build/plugins/websocketsource/CMakeFiles/websocketsourceplugin.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/tripzero/src/automotive-message-broker/plugins/websocketsourceplugin/websocketsource.cpp" "/home/tripzero/src/automotive-message-broker/build/plugins/websocketsourceplugin/CMakeFiles/websocketsourceplugin.dir/websocketsource.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "icecc")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "DBusServiceName=\"org.automotive.message.broker\""
+  "PROJECT_CODENAME=\"veyron\""
+  "PROJECT_NAME=\"automotive-message-broker\""
+  "PROJECT_QUALITY=\"alpha\""
+  "PROJECT_VERSION=\"0.11.800\""
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/tripzero/src/automotive-message-broker/build/lib/CMakeFiles/amb.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../lib"
+  "/usr/include/glib-2.0"
+  "/usr/lib/i386-linux-gnu/glib-2.0/include"
+  "/usr/include/json"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
