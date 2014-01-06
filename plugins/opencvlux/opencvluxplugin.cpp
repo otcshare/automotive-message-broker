86e6da80085c3a07b1c21cfb3c8cd7d08f52df88
Kevron Rees <kevron.m.rees@intel.com>
2014-01-06 13:51:18 -0800

    readded websocketsink plugin

--- plugins/opencvlux/opencvluxplugin.cpp
+++ plugins/opencvlux/opencvluxplugin.cpp
@@ -24,7 +24,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #include <boost/thread/future.hpp>
 
 #ifdef OPENCL
-#include <opencv/ocl/ocl.hpp>
+#include <opencv2/ocl/ocl.hpp>
 #endif
 
 #ifdef CUDA
