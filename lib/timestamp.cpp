7a8f35116d54c1b5b345908a43e8543203e258ba
Kevron Rees <kevron.m.rees@intel.com>
2014-04-10 16:45:50 -0700

    use monotonic clock

--- lib/timestamp.cpp
+++ lib/timestamp.cpp
@@ -6,7 +6,7 @@
 
 double amb::currentTime()
 {
-	auto tm = std::chrono::high_resolution_clock::now();
+	auto tm = std::chrono::steady_clock::now();
 
 	double time = std::chrono::duration_cast<std::chrono::milliseconds>(tm.time_since_epoch()).count() / 1000.00;
 
